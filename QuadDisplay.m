969231d848a31662dce130222b1ff3836b03de52
simondlevy <simon.d.levy@gmail.com>
2020-01-02 21:14:05 +0200

    Did some factoring

--- QuadDisplay.m
+++ QuadDisplay.m
@@ -6,7 +6,7 @@
 
 classdef QuadDisplay
 
-    properties(Constant, Access = private)
+    properties(Constant, Access=private)
 
         WORLD_SIZE    = 10; 
         VEHICLE_SIZE  = 3;
@@ -14,11 +14,11 @@ classdef QuadDisplay
 
     end
 
-    properties(Access = private)
+    properties(Access=private)
         d;
     end
 
-    methods(Access = public)
+    methods(Access=public)
 
         function obj = QuadDisplay
             s = obj.VEHICLE_SIZE / 2;
@@ -26,12 +26,31 @@ classdef QuadDisplay
         end
 
         function show(obj, x, y, z, phi, theta, psi)
-            plot3([x-obj.d,x+obj.d], [y-obj.d,y+obj.d], [z,z], obj.VEHICLE_COLOR)
+
+            p1 = [x-obj.d, y-obj.d, z];
+            p2 = [x+obj.d, y+obj.d, z];
+
+            p3 = [x-obj.d, y+obj.d, z];
+            p4 = [x+obj.d, y-obj.d, z];
+
+
+            obj.plotarm(p1, p2)
             hold on
-            plot3([x-obj.d,x+obj.d], [y+obj.d,y-obj.d], [z,z], obj.VEHICLE_COLOR)
+            obj.plotarm(p3, p4)
+
+            % Erase previous plot
             hold off
+
             axis([-obj.WORLD_SIZE obj.WORLD_SIZE -obj.WORLD_SIZE obj.WORLD_SIZE 0 obj.WORLD_SIZE])
             drawnow
+        end
+
+    end
+
+    methods(Access=private)
+
+        function plotarm(obj, p1, p2)
+            plot3([p1(1),p2(1)], [p1(2), p2(2)], [p1(3),p2(3)], obj.VEHICLE_COLOR)
         end
 
     end
