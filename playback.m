8ec7a708365f59da34d6aa1a4f8d7c892cb767f1
simondlevy <simon.d.levy@gmail.com>
2020-01-05 21:02:37 +0200

    Cleanup

--- playback.m
+++ playback.m
@@ -8,16 +8,7 @@
 %
 % MIT License
 
-function playback(a)
-
-    % Get values from matrix
-    tvals     = a(:,1);
-    xvals     = a(:,2);
-    yvals     = a(:,3);
-    zvals     = a(:,4);
-    phivals   = a(:,5);
-    thetavals = a(:,6);
-    psivals   = a(:,7);
+function playback(a, aviname)
 
     % Set up a quadcopter display
     qd = QuadDisplay;
@@ -28,6 +19,11 @@ function playback(a)
     % Start at first frame
     k = 1;
 
+    if nargin > 1
+        vw = VideoWrite(aviname);
+        open(vw);
+    end
+
     % Loop through the time values, interpolating as we go
     while true
 
@@ -35,18 +31,35 @@ function playback(a)
         t = toc;
 
         % If current time exceeds total time, were' done
-        if toc > tvals(end) 
+        if t > a(end,1) 
             break
         end
 
         % Find the frame closest to the current actual time
-        while tvals(k) < t
+        while a(k,1) < t
             k = k+1;
         end
 
-        % Display the quadcopter, negating Z for NED => ENU
-        qd.show(xvals(k), yvals(k), -zvals(k), phivals(k), thetavals(k), psivals(k))
+        % Display the current frame
+        qdshow(qd, k, a)
+
+        % Write to video file if indicated
+        if nargin > 1
+            writeVideo(vw, getframe(gcf));
+        end
 
     end
 
+    % Display the final frame
+    qdshow(qd, length(a), a)
+
+    % Cleanup
+    if nargin > 1
+        close(vw);
+    end
+
+end
+
+function qdshow(qd, k, a)
+    qd.show(a(k, 2), a(k,3), -a(k,4), a(k,5), a(k,6), a(k,7), sprintf('t=%3.2f/%3.2f', a(k,1), a(end,1)))
 end
