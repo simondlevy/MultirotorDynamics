f45ee3b3b6fdb3b0fcaaca801f421ba7b685be7b
simondlevy <simon.d.levy@gmail.com>
2020-01-05 18:45:44 +0200

    Removed live plot

--- takeoff.m
+++ takeoff.m
@@ -5,9 +5,7 @@
 %
 %   takeoff(dur, dt) runs for DUR seconds with an update period of DT seconds
 %
-%   takeoff(dt, dur, csvlog) also saves the results to file CSVLOG
-%
-%   takeoff(dur) displays in real time
+%   takeoff(dt, dur, file) also saves the results to a MAT file
 %
 % Copyright (C) 2019 Simon D. Levy
 %
@@ -25,15 +23,13 @@ function takeoff(dur, dt, csvlog)
     VEL_D = 0;
 
     % Time constant
-    if nargin < 1
-        fprintf('Usage: takeoff(duration, [dt], [csvlog]\n')
+    if nargin < 2
+        fprintf('Usage: takeoff(duration, [dt]\n')
         return
-    elseif nargin < 2
-        dt = 0;
-        q = QuadDisplay;
-    else
-        f = waitbar(0);
     end
+
+    % Create a wait-bar to display progress
+    wb = waitbar(0);
 
     % Create PID controller
     pid  = AltitudePidController(ALTITUDE_TARGET, ALT_P, VEL_P, VEL_I, VEL_D);
@@ -42,27 +38,22 @@ function takeoff(dur, dt, csvlog)
     dyn = DjiPhantomDynamics;
 
     % Initialize arrays for plotting
-    tvals = [];
-    uvals = [];
-    zvals = [];
-    vvals = [];
+    tvals = linspace(0, dur, dur*1/dt);
+    uvals = zeros(size(tvals));
+    zvals = zeros(size(tvals));
+    vvals = zeros(size(tvals));
 
     % Motors are initially off
     u = 0;
 
-    % Start timing
-    t = 0;
-    tprev = 0;
-    tic
-
     % Loop for duration
-    while t < dur
+    for k = 1:length(tvals)
         
         % Set all the motors to the value obtained from the PID controller
         dyn = dyn.setMotors(u*ones(1,4));
 
         % Update the dynamics
-        dyn = dyn.update(.001);
+        dyn = dyn.update(dt);
 
         % Get the current vehicle state vector
         s = dyn.getState();
@@ -76,45 +67,27 @@ function takeoff(dur, dt, csvlog)
         z     = -s(MultirotorDynamics.STATE_Z);
         v     = -s(MultirotorDynamics.STATE_Z_DOT);
 
-        msg = sprintf('%3.2f/%3.2f sec', t, dur);
-
-        if nargin > 1
-            t = t + dt;
-            waitbar(t/dur, f, msg)
-        else
-
-            % Update the timer
-            t = toc;
-            dt = t - tprev;
-            tprev = t;
-
-            % Show the vehicle
-            q.show(x, y, z, phi, theta, psi, msg)
-
-        end
+        % Update the wait-bar
+        t = tvals(k);
+        waitbar(t/dur, wb, sprintf('%3.2f/%3.2f sec', t, dur))
         
         % Get correction from PID controller
-        if dt > 0
-            u = pid.u(z, v, dt);
-        end
+        u = pid.u(z, v, dt);
 
         % Constrain correction to [0,1] to represent motor value
         u = max(0, min(1, u));
 
         % Track values
-        tvals = [tvals, t];
-        zvals = [zvals, z];
-        vvals = [vvals, v];
-        uvals = [uvals, u];
+        zvals(k) = z;
+        vvals(k) = v;
+        uvals(k) = u;
 
     end
 
-    if nargin > 1
-        close(f)
-    end
+    % Clsoe the wait-bar
+    close(wb)
 
     % Plot results
-    %figure
     make_subplot(tvals, zvals, 1, 'Altitude (m)', [0 ALTITUDE_TARGET+1])
     make_subplot(tvals, vvals, 2, 'Velocity (m/s)')
     make_subplot(tvals, uvals, 3, 'Motors', [-.1,1.1])
