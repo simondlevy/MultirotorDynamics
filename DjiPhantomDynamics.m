f0dd91488ceeeb89e1861e0dbb8ad1ee9e3356aa
simondlevy <simon.d.levy@gmail.com>
2020-01-06 04:45:54 -0500

    Added comments

--- DjiPhantomDynamics.m
+++ DjiPhantomDynamics.m
@@ -11,18 +11,19 @@ classdef DjiPhantomDynamics < QuadXAPDynamics
       function obj = DjiPhantomDynamics
           
               % Estimated
-              params.b = 5.E-06;
-              params.d = 2.E-06;
+              params.b = 5.E-06;  % force constatnt [F=b*w^2]
+              params.d = 2.E-06;  % torque constant [T=d*w^2]
   
               % https:%www.dji.com/phantom-4/info
-              params.m = 1.380; % kg
-              params.l = 0.350; % m
+              params.m = 1.380;   % mass [kg]
+              params.l = 0.350;   % arm length [m]
   
               % Estimated
-              params.Ix = 2;      
-              params.Iy = 2;      
-              params.Iz = 3;      
-              params.Jr = 38E-04; 
+              params.Ix = 2;      % [kg*m^2] 
+              params.Iy = 2;      % [kg*m^2] 
+              params.Iz = 3;      % [kg*m^2] 
+              params.Jr = 38E-04; % prop inertial [kg*m^2] 
+
               params.maxrpm = 15000;  
                         
               obj = obj@QuadXAPDynamics(params); 
