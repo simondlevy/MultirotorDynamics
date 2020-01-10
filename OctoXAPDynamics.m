c07328cb1c505d3ccc8a44119c09d277a1c5331a
Levy <levys@wlu.edu>
2020-01-10 17:22:09 -0500

    More mixer work

--- OctoXAPDynamics.m
+++ OctoXAPDynamics.m
@@ -15,7 +15,20 @@
 % MIT License
 
 classdef OctoXAPDynamics < MultirotorDynamics
-    
+
+    properties (Constant, Access=private)
+        mixer = [
+                [-1, -1, +1 ]; % 1
+                [+1, +1, +1 ]; % 2
+                [-1, -1, -1 ]; % 3
+                [-1, +1, -1 ]; % 4
+                [+1, -1, -1 ]; % 5
+                [+1, +1, -1 ]; % 6
+                [+1, -1, +1 ]; % 7
+                [-1, +1, +1 ]; % 8
+            ];
+    end
+
     properties(Access=private)
         C1;
         C2;
@@ -24,34 +37,13 @@ classdef OctoXAPDynamics < MultirotorDynamics
     methods
 
         function obj = OctoXAPDynamics(params, C1, C2)
-            obj = obj@MultirotorDynamics(params, 8);
+            obj = obj@MultirotorDynamics(params, OctoXAPDynamics.mixer);
             if nargin < 2
                 C1 = 1;
                 C2 = 1;
             end
             obj.C1 = C1;
             obj.C2 = C2;
-        end
-
-        function f = u2(obj,  o)
-            % roll right
-            f =  (obj.C1*o(2) + obj.C1*o(5) + obj.C2*o(6) + obj.C2*o(7)) - (obj.C1*o(1) + obj.C2*o(3) + obj.C1*o(4) + obj.C2*o(8));
-        end
-       
-        function f = u3(obj,  o)
-            % pitch forward
-            f = (obj.C2*o(2) + obj.C2*o(4) + obj.C1*o(6) + obj.C1*o(8)) - (obj.C2*o(1) + obj.C1*o(3) + obj.C2*o(5) + obj.C1*o(7));
-        end
-       
-        function f = u4(~,  o)
-            % yaw cw
-            f = (o(3) + o(4) + o(5) + o(6)) - (o(1) + o(2) + o(7) + o(8));
-        end
-
-        function d = motorDirection(i)
-            %motor direction for animation
-            dir = [+1, +1, -1, -1, -1, -1, +1, +1];
-            d = dir(i);
         end
 
     end
--- QuadXAPDynamics.m
+++ QuadXAPDynamics.m
@@ -19,34 +19,12 @@ classdef QuadXAPDynamics < MultirotorDynamics
             [+1,-1,+1]; 
             [-1,+1,+1]
             ];
-            
     end
   
     methods
 
         function obj = QuadXAPDynamics(params)
             obj = obj@MultirotorDynamics(params, QuadXAPDynamics.mixer);
-        end
-
-        function f = u2(~,  o)
-            % roll right
-            f = (o(2) + o(3)) - (o(1) + o(4));
-        end
-       
-        function f = u3(~,  o)
-            % pitch forward
-            f = (o(2) + o(4)) - (o(1) + o(3));
-        end
-       
-        function f = u4(~,  o)
-            % yaw cw
-            f = (o(1) + o(2)) -  (o(3) + o(4));
-        end
-
-        function d = motorDirection(i)
-            %motor direction for animation
-            dir = [-1, -1, +1, +1];
-            d = dir(i);
         end
 
     end
