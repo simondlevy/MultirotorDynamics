bc89897ee954a64d4537854993de11bec3fffa90
simondlevy <simon.d.levy@gmail.com>
2023-06-16 22:15:15 -0400

    Moved DJI Phantom vehicle params into its own folder

--- python/multirotor_dynamics/__init__.py
+++ python/multirotor_dynamics/__init__.py
@@ -343,23 +343,3 @@ class Dynamics:
         sps = np.sin(psi)
 
         return cph, cth, cps, sph, sth, sps
-
-
-djiphantom_params = {
-
-    # Estimated
-    'B': 5.E-06,  # force constatnt [F=b*w^2]
-    'D': 2.E-06,  # torque constant [T=d*w^2]
-
-    # https:#www.dji.com/phantom-4/info
-    'M': 1.380,  # mass [kg]
-    'L': 0.350,  # arm length [m]
-
-    # Estimated
-    'Ix': 2,       # [kg*m^2]
-    'Iy': 2,       # [kg*m^2]
-    'Iz': 3,       # [kg*m^2]
-    'Jr': 38E-04,  # prop inertial [kg*m^2]
-
-    'maxrpm': 15000
-}
